df3f95e66313b22a19debebd5dc18c62fc001f90
Goldy Bawa <deebawa48@gmail.com>
2025-03-09 18:52:34 +0530

    Minnor changes

--- nodejs-app/index.js
+++ nodejs-app/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Goldy'));
+app.get('/', (req, res) => res.send('Goldiii'));
 
 app.listen(3000, () => console.log('Example app listening on port http://localhost:3000'));
